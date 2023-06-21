e742b770f3e2cbd24ba305c9d2540eaaaaebeae7
Mikey <82703973+Bothub97@users.noreply.github.com>
2023-06-21 12:38:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def display_map(df, year, quarter):
     map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
     
     choropleth = folium.Choropleth(
-        geo_data='data/us-state-boundaries.geojson',
+        geo_data='us-state-boundaries.geojson',
         data=df,
         columns=('State Name', 'State Total Reports Quarter'),
         key_on='feature.properties.name',
